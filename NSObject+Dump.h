5e10562e2721e63f03f8495c2c865d022f857df8
Igor <altima@i.ua>
2016-07-11 19:10:08 +0300

    Add files via upload

---
+++ NSObject+Dump.h
@@ -0,0 +1,15 @@
+//
+//  NSObject+Dump.h
+//  kupiVip
+//
+//  Created by Igor Poltavtsev on 10.07.16.
+//  Copyright © 2016 kupivip.ru. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSObject (Dump)
+
+- (void)dumpObject:(id)object;
+
+@end
---
+++ NSObject+Dump.m
@@ -0,0 +1,63 @@
+//
+//  NSObject+Dump.m
+//  kupiVip
+//
+//  Created by Igor Poltavtsev on 10.07.16.
+//  Copyright © 2016 kupivip.ru. All rights reserved.
+//
+
+#import "NSObject+Dump.h"
+
+@implementation NSObject (Dump)
+
+- (void)dumpObject:(id)object {
+    
+#ifdef DEBUG
+    
+    NSString *string;
+    if ([object isKindOfClass:[NSArray class]])
+        string = ((NSArray*)object).description;
+    else if ([object isKindOfClass:[NSDictionary class]])
+        string = ((NSDictionary*)object).description;
+    else return;
+    
+    string = [string stringByReplacingOccurrencesOfString:@" " withString:@"@@**@@"];
+    NSScanner *scanner = [NSScanner scannerWithString:string];
+    NSMutableString *resultString = @"".mutableCopy;
+    NSString *buf = nil;
+    
+    while (![scanner isAtEnd] ) {
+        if ([scanner scanUpToString: @"\\U" intoString: &buf])
+            [resultString appendString: buf];
+        if ([scanner isAtEnd]) break;
+        [scanner setScanLocation: [scanner scanLocation] + 3]; //skip the '\U'
+        unsigned c = 0;
+        [resultString appendString: [scanner scanHexInt: &c] ? [NSString stringWithFormat:@"%C", (unichar)c] : @"\\U"];
+    }
+    string = [resultString stringByReplacingOccurrencesOfString:@"@@**@@" withString:@" "];
+    
+    resultString = [NSMutableString string];
+    scanner = [NSScanner scannerWithString:string];
+    
+    while (![scanner isAtEnd] ) {
+        if ([scanner scanUpToString: @"%" intoString: &buf])
+            [resultString appendString: buf];
+        if ([scanner isAtEnd]) break;
+        NSString *percent = [string substringWithRange:NSMakeRange([scanner scanLocation], 3)];
+        if (![percent stringByRemovingPercentEncoding])
+            percent = [string substringWithRange:NSMakeRange([scanner scanLocation], 6)];
+        if (![percent stringByRemovingPercentEncoding])
+            percent = [string substringWithRange:NSMakeRange([scanner scanLocation], 9)];
+        if (![percent stringByRemovingPercentEncoding])
+            percent = [string substringWithRange:NSMakeRange([scanner scanLocation], 12)];
+        [scanner setScanLocation: [scanner scanLocation] + percent.length]; //skip the '%'
+        [resultString appendString:[percent stringByRemovingPercentEncoding] ? [percent stringByRemovingPercentEncoding] : percent];
+    }
+    
+    NSLog(@"%@", resultString);
+    
+#endif
+    
+}
+
+@end

cc5e8119ff0006bbc4bcbe8cb8e6c0dd776db79f
Igor <altima@i.ua>
2016-07-11 19:17:11 +0300

    Update NSObject+Dump.m

--- NSObject+Dump.m
+++ NSObject+Dump.m
@@ -2,7 +2,7 @@
 //  NSObject+Dump.m
 //
 //  Created by Igor Poltavtsev on 10.07.16.
-//  Copyright © 2016 kupivip.ru. All rights reserved.
+//  Copyright © 2016 Igor Poltavtsev. All rights reserved.
 //
 
 #import "NSObject+Dump.h"

54708bc0af58d70d7c153f92737c803af360ae99
Igor <altima@i.ua>
2016-07-11 19:17:26 +0300

    Update NSObject+Dump.h

--- NSObject+Dump.h
+++ NSObject+Dump.h
@@ -2,7 +2,7 @@
 //  NSObject+Dump.h
 //
 //  Created by Igor Poltavtsev on 10.07.16.
-//  Copyright © 2016 Igor Poltavtsev All rights reserved.
+//  Copyright © 2016 Igor Poltavtsev. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
